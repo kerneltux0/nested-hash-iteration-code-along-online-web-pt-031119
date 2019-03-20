04d8785ac40b14426a180dc5bf76953634dc839a
Ryan Sise <kerneltux0@gmail.com>
2019-03-20 15:20:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,8 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |name,data|
     data.each do |aspect,details|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      if aspect == :favorite_ice_cream_flavors
+        details.delete_if {|ice_cream| ice_cream == "strawberry"}
     end
   end
 end
